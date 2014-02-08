676da981da2bf249ff92fc5474d2b7f568aabc2f
mdkvimer <mengdaikun@gmail.com>
2014-02-08 12:44:54 +0800

    first init

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 zabbixapi
 =========
 
+利用python初始化zabbix auth 
+
+调用zabbix api前的认证令牌的获取
+
+利用获取的认证令牌，调用zabbix api 获取zabbix server hosts 列表
+
 python opeartion zabbix api
---
+++ get_zabbix_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#coding=utf-8
+import json
+import urllib2
+# based url and required header
+url = "http://10.131.10.73/zabbix/api_jsonrpc.php"
+header = {"Content-Type": "application/json"}
+# auth user and password
+data = json.dumps(
+{
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+    "user": "admin",
+    "password": "zabbix"
+},
+"id": 0
+})
+# create request object
+request = urllib2.Request(url,data)
+for key in header:
+    request.add_header(key,header[key])
+# auth and get authid
+try:
+    result = urllib2.urlopen(request)
+except IOError , e:
+    print "Auth Failed, Please Check Your Name And Password:",e.code
+else:
+    response = json.loads(result.read())
+    result.close()
+    print "Auth Successful. The Auth ID Is:",response['result']
